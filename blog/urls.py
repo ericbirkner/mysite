f7acf76b8401a3a874b34b20ff84248c3c9d813c
Eric Birkner <ericbirkner@gmail.com>
2017-08-24 18:41:27 -0300

    se sumo el campo imagen

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     url(r'^$', views.post_list),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
